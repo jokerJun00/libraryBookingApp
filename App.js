b522c2c09260470a039536cc8a9293f29ffdba69
Loh Yong Bin <lohyongbin0819@gmail.com>
2022-08-16 21:57:30 +0800

    update app.js and booklistscreen.js

--- App.js
+++ App.js
@@ -11,59 +11,68 @@ import {
   Button,
 } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { back } from 'react-native/Libraries/Animated/Easing';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { LogBox } from "react-native";
 import 'react-native-gesture-handler';
+LogBox.ignoreLogs(["EventEmitter.removeListener"]);
 
 import HomeScreen from './frontend/screens/HomeScreen';
 import BookListScreen from './frontend/screens/BookListScreen';
 import BookingScreen from './frontend/screens/BookingScreen';
 import BookDetailScreen from './frontend/screens/BookDetailScreen';
+import UserProfileScreen from './frontend/screens/UserProfileScreen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
+const MyStack=()=>{
+  return(
+    <Stack.Navigator
+      initialRouteName='Home'
+      screenOptions={{
+        headerShown: false,
+        headerTitleStyle: {
+          fontSize: 20,
+          fontFamily: 'PlayfairDisplay-Bold',
+        },
+        headerStyle: {
+          backgroundColor: '#F9F5EB',
+          paddingLeft: 20,
+        },
+        headerTintColor: '#000',
+      }}>
+      <Stack.Screen
+        name='Home'
+        component={HomeScreen}></Stack.Screen>
+      <Stack.Screen
+        name='BookList'
+        component={BookListScreen}></Stack.Screen>
+      <Stack.Screen
+        name='BookDetail'
+        component={BookDetailScreen}></Stack.Screen>
+      <Stack.Screen
+        name='Booking'
+        component={BookingScreen}
+        options={{
+          headerLeft: () => (
+            <BackButton/>
+          ),
+        }}></Stack.Screen>
+    </Stack.Navigator>
+  )
+}
+  
 export default class App extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName='Home'
-          screenOptions={{
-            headerShown: false,
-            headerTitleStyle: {
-              fontSize: 20,
-              fontFamily: 'PlayfairDisplay-Bold',
-            },
-            headerStyle: {
-              backgroundColor: '#F9F5EB',
-              paddingLeft: 20,
-            },
-            headerTintColor: '#000',
-          }}
-        >
-          <Stack.Screen
-            name='Home'
-            component={HomeScreen}
-          />
-          <Stack.Screen
-            name='BookList'
-            component={BookListScreen}
-          />
-          <Stack.Screen
-            name='BookDetail'
-            component={BookDetailScreen}
-          />
-          <Stack.Screen
-            name='Booking'
-            component={BookingScreen}
-            options={{
-              headerLeft: () => (
-                <BackButton/>
-              ),
-            }}
-          />
-        </Stack.Navigator>
+        <Tab.Navigator initialRouteName={'First Screen'}>
+          <Tab.Screen name="First Screen" component={MyStack} options={{ headerShown: false, tabBarIcon:()=>{return(<Ionicons name='home' size={20} color={'blue'}></Ionicons>)} }}></Tab.Screen>
+          <Tab.Screen name="Profile" component={UserProfileScreen} options={{ tabBarIcon:()=>{return(<Ionicons name='person' size={20} color={'blue'}></Ionicons>)}}}></Tab.Screen>
+        </Tab.Navigator>
       </NavigationContainer>
     );
   }
--- frontend/screens/BookListScreen.js
+++ frontend/screens/BookListScreen.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, ScrollView, View, Button } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  ScrollView,
+  Image,
+  Button,
+} from 'react-native';
 
+const Separator = () => (
+  <View style={styles.separator} />
+);
 
-export default class BookListScreen extends Component {
+export default class BookListScreen extends Component { 
+  constructor(props){
+    super(props);
+    this.state = {
+      bookTitle: "Rich Dad, Poor Dad",
+      bookAuthor: "Robert Kiyosaki, Sharon Lechter",
+    };
+  }
+
   render() {
     return(
+      <ScrollView>
       <View style={styles.container}>
-        <Text>This is Book List Screen</Text>
-        <Button
-          onPress={() => {
-            this.props.navigation.navigate('BookDetail');
-          }}
-          title="Book Detail"
-        ></Button>
+        <Image
+          style={styles.img}
+          source={require('../../assets/images/books-images/rich-dad-poor-dad.jpeg')}
+        />
+        <View style={styles.columnContainer}>
+          <Text style={styles.Title}>
+            {this.state.bookTitle}{'\n'}
+          </Text>
+          <Text style={styles.Author}>
+            {this.state.bookAuthor}{'\n'}{'\n'}
+          </Text>
+          <View style={styles.buttons}>
+            <Button 
+              title="Book Detail"
+              onPress={() => {
+                this.props.navigation.navigate('BookDetail');
+              }}
+            />
+          </View>
+        </View>
+        <Separator />
       </View>
+      </ScrollView>
     );
   }
 }
@@ -22,7 +56,41 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#F9F5EB',
     justifyContent: 'center',
-    alignItems: 'center',
     flex: 1,
-  }
+    flexDirection:'row',
+    alignItems: 'center',
+  },
+  columnContainer: {
+    flexDirection: 'column',
+    paddingTop: 10,
+    paddingRight: 10,
+    justifyContent: 'center',
+    flex: 1,
+  },
+  img: {
+    alignSelf: 'flex-start',
+    width: 140,
+    height: 190,
+    margin: 20,
+    borderRadius: 5,
+  },
+  Title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  Author: {
+    fontSize: 15,
+  },
+  buttons: {
+    width: 140,
+    height: 40,
+    borderRadius: 15,
+    paddingRight: 0,
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
 })
+
